842ea9a1ecff2017c0827e8e38fbcaf60c81ed49
Julif <julif-@live.com>
2019-12-25 16:46:52 -0300

    fixes

--- sw.js
+++ sw.js
@@ -1,19 +1,19 @@
 // use a cacheName for cache versioning
 var cacheName = 'v1:static';
+// new Request('https://farm6.staticflickr.com/5594/14749918329_888df4f2ef.jpg', {mode: 'no-cors'})
 
 // during the install phase you usually want to cache static assets
 self.addEventListener('install', function(e) {
     // once the SW is installed, go ahead and fetch the resources to make this work offline
     e.waitUntil(
-        caches.open(cacheName).then(function(cache) {
-            return cache.addAll([
+        caches.open(cacheName).then(async function(cache) {
+           /*  await cache.addAll([
                 './',
                 './css/style.css',
                 './css/fonts/roboto.woff',
                 './offline.html'
-            ]).then(function() {
-                self.skipWaiting();
-            });
+            ]);
+            self.skipWaiting(); */
         })
     );
 });
